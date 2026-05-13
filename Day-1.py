5940408ec1e7955f5236e1159c23e6e48439cff8
LIKHITH <125430200+Likhithguthula@users.noreply.github.com>
2026-05-13 15:35:30 +0530

    Add files via upload

---
+++ Day-1.py
@@ -0,0 +1,52 @@
+print("This is my first python class")
+'''
+what is python ?
+-High-Level Language
+---> No need o worry about low-level management
+-Dynamically Language
+---> No need to mention the kind of data aking to variable
+-Interperter Language
+---> Python is exucuting libe-by-line and can through the error if any but all the before line will execute and gives output.
+'''
+num = 3
+so = "python"
+any = [1,2]
+an = {"Name :" , "Hi"}
+print(num)
+print(so)
+print(any)
+print(an)
+
+'''
+Tokens
+-------
+if
+else
+while
+for
+
+Identifiers
+------------
+Name of variables, function and classes.
+good ways
+----------
+A-Z, a-z, _
+bad ways
+---------
+special characters, space, 0-9
+'''
+
+'''
+Boolean
+--------
+True or False
+'''
+num_1 = 9
+num_2 = 7
+print(num_1 == num_2)
+
+
+
+
+
+
